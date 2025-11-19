6814b12c109bf67051db3a77c0099ffa1782028b
Wafiul Achdi <achdiwafiul@gmail.com>
2025-11-19 21:33:56 +0700

    feat(backend): initialize Hono API server with Bun and setup Prisma ORM

---
+++ backend/prisma/migrations/20251118074959_users/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `username`(`username`),
+    UNIQUE INDEX `email`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
